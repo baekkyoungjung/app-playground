3fd6607e8e8bd6a6055b49a669bbd3be48b9011e
bk <baekkyoungjung@gmail.com>
2023-01-11 10:29:29 +0900

    docs: 추가

--- app/kmong/SegmentConfigs.tsx
+++ app/kmong/SegmentConfigs.tsx
@@ -3,7 +3,9 @@ import React, { use } from 'react';
 /**
  * @see https://beta.nextjs.org/docs/api-reference/segment-config
  */
-// dynamic이 'force-dynamic' 이라면 항상 SSR
+/**
+ * @param dynamic 'auto' | 'force-dynamic' | 'error' | 'force-static'
+ */
 export const dynamic = 'auto';
 export const dynamicParams = true;
 // revalidate 가 0이라면 항상 SSR
